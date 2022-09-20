5f9c311a5f31e0ad08eec1509925beb2ff7ad952
roji <180122roji@gmail.com>
2022-09-20 12:31:33 -0400

    forgetpass

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -151,7 +151,12 @@ class _MyLoginState extends State<MyLogin> {
                                 style: ButtonStyle(),
                               ),
                               TextButton(
-                                  onPressed: () {},
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(builder: (context) =>  MyRegister()),
+                                    );
+                                  },
                                   child: Text(
                                     'Forgot Password',
                                     style: TextStyle(
